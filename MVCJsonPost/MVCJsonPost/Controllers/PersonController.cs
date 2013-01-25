658aed44bf7686a9c571fff4ae218423c5e1f585
Santi Murtagh <santiago.murtagh@gmail.com>
2013-01-24 22:22:45 -0600

    creating project sample

---
+++ MVCJsonPost/MVCJsonPost/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCJsonPost.Models;
+
+namespace MVCJsonPost.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new PersonModel());
+        }
+
+        [HttpPost]
+        public JsonResult BadSave(string first, string last, List<string> favoriteBands)
+        {
+            return Json(new { result = "saved the bad way" });
+        }
+
+        [HttpPost]
+        public JsonResult GoodSave(PersonModel model)
+        {
+            return Json(new { result = "saved the good way" });
+        }
+    }
+}
---
+++ MVCJsonPost/MVCJsonPost/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages.Html;
+
+namespace MVCJsonPost.Models
+{
+    public class PersonModel
+    {
+        [DisplayName("First")]
+        public string First { get; set; }
+
+        [DisplayName("Last")]
+        public string Last { get; set; }
+
+        [DisplayName("Favorite Bands")]
+        public List<string> FavoriteBands { get; set; }
+    }
+}
